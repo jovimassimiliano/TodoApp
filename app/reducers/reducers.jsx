4e62f221190b3bc4a18bbd3bc2b0570525a85af5
jovimassimiliano <massimiliano.giovanni@yahoo.com>
2017-05-03 12:27:25 +0700

    add reducers

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require("node-uuid");
+var moment = require("moment");
+
 export var searchTextReducer = (state = "",action) => {
   switch (action.type) {
     case "SET_SEARCH_TEXT":
@@ -13,6 +16,37 @@ export var showCompletedReducer = (state = false,action) => {
     case "TOGGLE_SHOW_COMPLETED":
       return !state;
       break;
+    default:
+      return state;
+  }
+};
+
+export var todoReducer = (state = [],action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return[
+        ...state,
+        {
+          id:uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+      break;
+    case "TOGGLE_TODO":
+      return state.map((todo) => {
+        if(todo.id === action.id){
+          var completed = !todo.completed;
+          return{
+            ...todo,
+            completed: completed,
+            completedAt: completed ? moment().unix() : undefined
+          };
+        }
+      });
+
     default:
       return state;
   }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,40 @@ describe("reducers", () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe("todosReducer", () => {
+    it("should add new todo", () => {
+      var action = {
+        type: "ADD_TODO",
+        text: "hello"
+      };
+
+      var res = reducers.todoReducer(df([]),df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it("should toggle toggle", () => {
+      var todos = [{
+        id: "123",
+        type: "TOGGLE_TODO",
+        text: "something",
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: "TOGGLE_TODO",
+        id: "123"
+      };
+
+      var res = reducers.todoReducer(df(todos),df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].completedAt).toEqual(undefined);
+      expect(res[0].completed).toEqual(false);
+    });
+  });
+
 });
